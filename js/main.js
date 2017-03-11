519126d767161dfdbf7937616a0d072a02017eed
shannabarnard <shannabarnard@gmail.com>
2017-03-11 10:10:30 +0800

    add animate css

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
   $('.owl-carousel').owlCarousel({
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
     loop:true,
     margin:10,
     autoplay: 500,
     items:1
-})
+  })
 });
